be4db41622bed643eeaa2d4ce384d98a5ef5e42f
coolfurui <coolfurui@gmail.com>
2025-03-13 12:08:16 -0400

    尝试修复菜单页面的图片加载问题

--- client/src/pages/Menu.tsx
+++ client/src/pages/Menu.tsx
@@ -1,6 +1,25 @@
 import React, { useState } from "react";
 import { Row, Col, Nav } from "react-bootstrap";
 
+// 导入菜单图片
+import all1 from "../assets/menu/all-1.jpg";
+import all2 from "../assets/menu/all-2.jpg";
+import all3 from "../assets/menu/all-3.jpg";
+import sandwich1 from "../assets/menu/sandwich-1.jpg";
+import sandwich2 from "../assets/menu/sandwich-2.jpg";
+import sandwich3 from "../assets/menu/sandwich-3.jpg";
+import drink1 from "../assets/menu/drink-1.jpg";
+import drink2 from "../assets/menu/drink-2.jpg";
+import drink3 from "../assets/menu/drink-3.jpg";
+import bread1 from "../assets/menu/bread-1.jpg";
+import bread2 from "../assets/menu/bread-2.jpg";
+import bread3 from "../assets/menu/bread-3.jpg";
+import bread4 from "../assets/menu/bread-4.jpg";
+import bread5 from "../assets/menu/bread-5.jpg";
+import bread6 from "../assets/menu/bread-6.jpg";
+import set1 from "../assets/menu/set-1.jpg";
+import set2 from "../assets/menu/set-2.jpg";
+
 // 定义菜品类型接口
 interface MenuItem {
   id: string;
@@ -23,21 +42,21 @@ const menuData: MenuData = {
       name: "热烤热压三明治",
       description: "每一天的美好开始",
       price: "¥6-12",
-      image: "/src/assets/menu/all-1.jpg",
+      image: all1,
     },
     {
       id: "all2",
       name: "特色饮品",
       description: "悠闲时光的最佳搭配",
       price: "¥6",
-      image: "/src/assets/menu/all-2.jpg",
+      image: all2,
     },
     {
       id: "all3",
       name: "手作面包",
       description: "深受顾客喜爱的组合",
       price: "¥6-12",
-      image: "/src/assets/menu/all-3.jpg",
+      image: all3,
     },
   ],
   sandwich: [
@@ -46,21 +65,21 @@ const menuData: MenuData = {
       name: "火腿咸香三明治",
       description: "精选火腿，搭配新鲜生菜，口感丰富",
       price: "¥10",
-      image: "/src/assets/menu/sandwich-1.jpg",
+      image: sandwich1,
     },
     {
       id: "s2",
       name: "鸡蛋三明治",
       description: "嫩滑鸡蛋，搭配特制酱料",
       price: "¥10",
-      image: "/src/assets/menu/sandwich-2.jpg",
+      image: sandwich2,
     },
     {
       id: "s3",
       name: "牛肉芝士三明治",
       description: "澳洲进口牛肉，搭配香浓芝士",
       price: "¥10",
-      image: "/src/assets/menu/sandwich-3.jpg",
+      image: sandwich3,
     },
   ],
   drinks: [
@@ -69,21 +88,21 @@ const menuData: MenuData = {
       name: "斑斓椰奶",
       description: "精选茶底，搭配进口奶制品",
       price: "¥6",
-      image: "/src/assets/menu/drink-1.jpg",
+      image: drink1,
     },
     {
       id: "d2",
       name: "水果茶",
       description: "新鲜水果，清爽怡人",
       price: "¥6",
-      image: "/src/assets/menu/drink-2.jpg",
+      image: drink2,
     },
     {
       id: "d3",
       name: "美式咖啡",
       description: "醇香浓郁，提神醒脑",
       price: "¥6",
-      image: "/src/assets/menu/drink-3.jpg",
+      image: drink3,
     },
   ],
   bread: [
@@ -92,42 +111,42 @@ const menuData: MenuData = {
       name: "牛角面包",
       description: "黄油香酥，层次分明",
       price: "¥12",
-      image: "/src/assets/menu/bread-1.jpg",
+      image: bread1,
     },
     {
       id: "b2",
       name: "吐司面包",
       description: "松软可口，早餐首选",
       price: "¥8",
-      image: "/src/assets/menu/bread-2.jpg",
+      image: bread2,
     },
     {
       id: "b3",
       name: "全麦面包",
       description: "健康营养，粗粮主食",
       price: "¥10",
-      image: "/src/assets/menu/bread-3.jpg",
+      image: bread3,
     },
     {
       id: "b4",
       name: "法式长棍",
       description: "外酥内软，经典法式",
       price: "¥15",
-      image: "/src/assets/menu/bread-4.jpg",
+      image: bread4,
     },
     {
       id: "b5",
       name: "肉松面包",
       description: "松软可口，美味营养",
       price: "¥9",
-      image: "/src/assets/menu/bread-5.jpg",
+      image: bread5,
     },
     {
       id: "b6",
       name: "芝士面包",
       description: "浓郁芝士，香甜可口",
       price: "¥11",
-      image: "/src/assets/menu/bread-6.jpg",
+      image: bread6,
     },
   ],
   sets: [
@@ -136,14 +155,14 @@ const menuData: MenuData = {
       name: "早餐套餐A",
       description: "三明治+奶茶+水果",
       //price: "¥28",
-      image: "/src/assets/menu/set-1.jpg",
+      image: set1,
     },
     {
       id: "set2",
       name: "下午茶套餐",
       description: "面包+饮品+小食",
       //price: "¥32",
-      image: "/src/assets/menu/set-2.jpg",
+      image: set2,
     },
   ],
 };
